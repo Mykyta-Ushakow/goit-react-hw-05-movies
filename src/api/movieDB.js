a0f34123ebe7716534cc5c7eff4f40230c3039d0
Mykyta-Ushakow <nilitosushak@gmail.com>
2023-12-07 15:58:09 +0200

    setting up structure

---
+++ src/api/movieDB.js
@@ -0,0 +1,44 @@
+// const API_KEY = 'ba5bcc67972e357b939718a8ae792a34';
+
+// const READ_TOKEN =
+//   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTViY2M2Nzk3MmUzNTdiOTM5NzE4YThhZTc5MmEzNCIsInN1YiI6IjY1NzFiNDYyZGZlMzFkMDBlMGRhODRjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yPHj6OaU0BrsomrkwryTdS1cjcHudho8uvc6soYTGJ0';
+
+async function getTrending() {
+  const API_KEY = 'ba5bcc67972e357b939718a8ae792a34';
+
+  const READ_TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTViY2M2Nzk3MmUzNTdiOTM5NzE4YThhZTc5MmEzNCIsInN1YiI6IjY1NzFiNDYyZGZlMzFkMDBlMGRhODRjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yPHj6OaU0BrsomrkwryTdS1cjcHudho8uvc6soYTGJ0';
+
+  const PARAMS = new URLSearchParams({
+    API_KEY,
+    append_to_response: 'images',
+  });
+}
+
+async function searchMovies() {
+  const API_KEY = 'ba5bcc67972e357b939718a8ae792a34';
+
+  const READ_TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTViY2M2Nzk3MmUzNTdiOTM5NzE4YThhZTc5MmEzNCIsInN1YiI6IjY1NzFiNDYyZGZlMzFkMDBlMGRhODRjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yPHj6OaU0BrsomrkwryTdS1cjcHudho8uvc6soYTGJ0';
+}
+
+async function getMovieDetails() {
+  const API_KEY = 'ba5bcc67972e357b939718a8ae792a34';
+
+  const READ_TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTViY2M2Nzk3MmUzNTdiOTM5NzE4YThhZTc5MmEzNCIsInN1YiI6IjY1NzFiNDYyZGZlMzFkMDBlMGRhODRjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yPHj6OaU0BrsomrkwryTdS1cjcHudho8uvc6soYTGJ0';
+}
+
+async function getMovieCredits() {
+  const API_KEY = 'ba5bcc67972e357b939718a8ae792a34';
+
+  const READ_TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTViY2M2Nzk3MmUzNTdiOTM5NzE4YThhZTc5MmEzNCIsInN1YiI6IjY1NzFiNDYyZGZlMzFkMDBlMGRhODRjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yPHj6OaU0BrsomrkwryTdS1cjcHudho8uvc6soYTGJ0';
+}
+
+async function getMovieReviews() {
+  const API_KEY = 'ba5bcc67972e357b939718a8ae792a34';
+
+  const READ_TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTViY2M2Nzk3MmUzNTdiOTM5NzE4YThhZTc5MmEzNCIsInN1YiI6IjY1NzFiNDYyZGZlMzFkMDBlMGRhODRjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yPHj6OaU0BrsomrkwryTdS1cjcHudho8uvc6soYTGJ0';
+}
